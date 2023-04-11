8ba585bb059084251e9e62b146329369857e354c
pfzoz <pedrozoz.sizaan@gmail.com>
2023-04-10 23:05:14 -0300

    first-commit

--- src/domains/blog/blog.controller.ts
+++ src/domains/blog/blog.controller.ts
@@ -1,9 +1,28 @@
 import { Router, Request, Response } from "express"
 
+class Prontuary {
+    patient_name : string
+    patient_data : Map<string, string>
+
+    constructor(patient_name : string, patient_data : Map<string, string>) {
+        this.patient_name = patient_name
+        this.patient_data = patient_data
+    }
+}
+
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
     return res.send("AAAA")
+})
+
+router.get("/prontuary", (req: Request, res: Response) => {
+    const isAuthenticated = false;
+    if (isAuthenticated) {
+        // TODO GET DADOS
+    } else {
+        return res.send("É necessário fazer log-in para acessar seus prontuários.")
+    }
 })
 
 router.get('/:id', (req: Request, res: Response) => {
