509b1600afb75ee6eb40cdcf5ba49306ecb22241
pfzoz <pedrozoz.sizaan@gmail.com>
2023-04-15 11:02:37 -0300

    db-connection-and-objects

--- src/domains/prontuary/prontuary.controller.ts
+++ src/domains/prontuary/prontuary.controller.ts
@@ -1,25 +1,38 @@
 import { Router, Request, Response } from "express";
 import { readFile, readFileSync } from "fs";
+import path from "path";
 
-class Prontuary {
-  patient_name: String;
-  patient_data: Object;
-
-  constructor(patient_name: string, patient_data: Object) {
-    this.patient_name = patient_name;
-    this.patient_data = patient_data;
-  }
-}
+import { Pool, PoolClient } from "pg";
+import { ProntuaryObject } from "./prontuary_class/prontuary"
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response) => {
   // TODO GET DADOS
-  return res.send("AAAA")
-  return res.send(() => {
-    let data = readFileSync("prontuary_template.json", "utf-8")
-    return data
-  })
+  const usuario_id = 1;
+  const pool = new Pool({
+    connectionString:
+      "postgres://postgres:OLOSISTOTAL7777777!@localhost:5432/myclinic",
+  });
+  const connect = pool.connect();
+  connect.then(async (client: PoolClient) => {
+    const query_res = await client.query(
+      `SELECT * FROM prontuaries WHERE user_id = ${usuario_id}`
+    );
+    // console.log(query_res.rows);
+    let e = {json_path: "hey"}
+    res.send(query_res.rows);
+    query_res.rows.forEach((value : ProntuaryObject, index) => {
+      console.log(value.json_path)
+    })
+    client.release();
+  });
+  // const pront_path = path.join(__dirname, "prontuary_template.json")
+  // readFile(pront_path, "utf-8", (err, data) => {
+  //   if (err != null) throw err
+
+  //   return res.send(data)
+  // })
 });
 
 export default router;
