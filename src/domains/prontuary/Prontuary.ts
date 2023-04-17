325ffeec43313659ddda80c4cbfe7f81fb7b1151
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2023-04-17 16:40:50 -0300

    revert

---
+++ src/domains/prontuary/Prontuary.ts
@@ -0,0 +1,11 @@
+import User from "../user/User";
+
+export default class Prontuary {
+    patient_name
+    patient_data
+  
+    constructor(patient_name: string, patient_data: Object) {
+      this.patient_name = patient_name;
+      this.patient_data = patient_data;
+    }
+  }
