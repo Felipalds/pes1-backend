7cd7067e509d09aee2305644f7812bb24f79492a
pfzoz <pedrozoz.sizaan@gmail.com>
2023-04-19 21:36:01 -0300

    full-crud

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 import bodyParser from 'body-parser';
 import routes from './routes/routes';
 
+const cors = require("cors")
+
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT;
 
-
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended:false }))
 app.use(bodyParser.json())
 app.use('/', routes)
