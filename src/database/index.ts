e3f2f23e5bcc6b232bb31972683d3c272c0b2c1c
pfzoz <pedrozoz.sizaan@gmail.com>
2023-04-25 00:51:18 -0300

    oop-adaptation

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,6 +2,67 @@ import { Pool, PoolClient } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
+// Classes
+
+class Prontuary {
+  private user_id : string
+  private title : string
+
+  constructor(user_id : any = "all", title : any = "all") {
+    this.user_id = (user_id === "all") ? "" : `user_id = ${user_id} AND `
+    this.title = (title === "all") ? "" : title
+  }
+
+  public set(user_id : any, title : any) {
+    this.user_id = (user_id === "all") ? "" : `user_id = ${user_id} AND `
+    this.title = (title === "all") ? "" : title
+  }
+
+  public set_user_id(user_id : any) {
+    this.user_id = (user_id === "all") ? "" : `user_id = ${user_id} AND `
+  }
+
+  public set_title(title : any) {
+    this.title = (title === "all") ? "" : title
+  }
+
+  public async get(n : any) {
+    const client = await pool.connect()
+    const limit = (n === "all") ? "" : `LIMIT ${n}`
+    const query = `SELECT * FROM prontuaries 
+    WHERE ${this.user_id} title ILIKE '%${this.title}%' ${limit}`
+    const query_res = await client.query(query)
+    client.release()
+    return query_res.rows
+  }
+
+  public async create(title : any) {
+    const client = await pool.connect()
+    const check_query = `SELECT * FROM prontuaries 
+    WHERE ${this.user_id} title = '${title}'`
+    const check_res = await client.query(check_query)
+    if (check_res.rows.length > 0) {
+      client.release()
+      return `Insertion Failed, title specified already exists for user with id '${this.user_id}".`
+    } else {
+      const json_path = `${this.title}_${this.user_id}.json`
+      const insert_query = `INSERT INTO prontuaries(user_id, title, json_path)
+      VALUES(${this.user_id.split(' ')[2]}, '${title}', '${json_path}')`
+      await client.query(insert_query)
+      client.release()
+      return "Success."
+    }
+  }
+
+  public async delete(table_id : any) {
+    const client = await pool.connect()
+    const query = `DELETE FROM prontuaries WHERE ${this.user_id} table_id = ${table_id}`
+    await client.query(query)
+    client.release()
+    return "Success."
+  }
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
@@ -11,47 +72,6 @@ console.log(process.env.DATABASE_URL);
 pool.on("connect", () => {
   console.log("Database connected!");
 });
-
-// setters
-
-const createProfessional = async (user_id: any, title: any) => {
-  const client = await pool.connect();
-  let check_query = `SELECT * FROM prontuaries WHERE title = '${title}'`;
-  const check_res = await client.query(check_query);
-  if (check_res.rows.length > 0) {
-    return `Insertion Failed, title specified already exists for user with id '${user_id}'.`;
-  } else {
-    let json_path = `${title}_${user_id}.json`;
-    let insert_query = `INSERT INTO prontuaries(user_id, title, json_path)
-        VALUES(${user_id}, '${title}', '${json_path}')`;
-    await client.query(insert_query);
-    return "Success.";
-  }
-};
-
-const deleteProfessional = async (user_id: any, table_id: any) => {
-    const client = await pool.connect()
-    const query = `DELETE FROM prontuaries WHERE user_id = ${user_id} AND table_id = ${table_id}`
-    await client.query(query)
-    return "Success."
-}
-
-// getters
-
-const getUserById = async (
-  user_id: any = "all",
-  title: any = "",
-  n: any = "all"
-) => {
-  const client = await pool.connect();
-  const limit = n === "all" ? "" : `LIMIT ${n}`;
-  const users = user_id === "all" ? "" : `user_id = ${user_id} AND `
-  let query = `SELECT * FROM prontuaries WHERE 
-                ${users} title ILIKE '%${title}%' ${limit}`;
-  const query_res = await client.query(query);
-  client.release();
-  return query_res.rows;
-};
 
 const getAllProcedures = async () => {
   const client = await pool.connect();
@@ -84,11 +104,9 @@ const getProfessionalById = async (professionalId: any) => {
 };
 
 export {
-  getUserById,
   getAllProcedures,
   getProcedureById,
   getAllProfessionals,
   getProfessionalById,
-  createProfessional,
-  deleteProfessional,
+  Prontuary,
 };
--- src/domains/prontuary/prontuary.controller.ts
+++ src/domains/prontuary/prontuary.controller.ts
@@ -1,33 +1,51 @@
 import { Router, Request, Response } from "express";
-import { getUserById, createProfessional, deleteProfessional} from "../../database";
-import { create } from "domain";
+import {
+  Prontuary
+} from "../../database";
 
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  const user_id = Number.isNaN(Number(req.query.user_id)) ? "all" : Number(req.query.user_id);
+  const user_id = Number.isNaN(Number(req.query.user_id))
+    ? "all"
+    : Number(req.query.user_id);
   const title = req.query.title;
-  const n = Number.isNaN(Number(req.query.n)) ? "all" : Number(req.query.n)
-  const answer = getUserById(user_id, title, n);
-  answer.then((rows) => {
-    res.send(rows);
-  });
+  const n = Number.isNaN(Number(req.query.n)) ? "all" : Number(req.query.n);
+  const prontuary = new Prontuary(user_id, title)
+  prontuary.get(n)
+    .then((rows) => {
+      res.send(rows);
+    })
+    .catch((error) => {
+      console.error(`Error while getting prontuaries: ${error}`);
+      return res.send({});
+    });
 });
 
 router.post("/create", (req: Request, res: Response) => {
   const { user_id, title } = req.body;
-  const answer = createProfessional(user_id, title);
-  answer.then((msg) => {
-    res.send(msg);
-  });
+  const prontuary = new Prontuary(user_id)
+  prontuary.create(title)
+    .then((message) => {
+      res.send(message);
+    })
+    .catch((error) => {
+      console.error(`Error while creating prontuaries: ${error}`);
+      res.send("Fail.");
+    });
 });
 
 router.post("/delete", (req: Request, res: Response) => {
-  const { user_id, table_id } = req.body
-  const answer = deleteProfessional(user_id, table_id)
-  answer.then((msg) => {
-    res.send(msg)
-  })
+  const { user_id, table_id } = req.body;
+  const prontuary = new Prontuary(user_id)
+  prontuary.delete(table_id)
+    .then((message) => {
+      res.send(message);
+    })
+    .catch((error) => {
+      console.error(`Error while deleting prontuaries`);
+      res.send("Fail.");
+    });
 });
 
 export default router;
