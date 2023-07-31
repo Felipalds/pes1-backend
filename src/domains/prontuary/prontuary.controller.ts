db7d1d1bb893f5d4e4e3e38f7e60f48a39a43248
pfzoz <pedrozoz.sizaan@gmail.com>
2023-07-31 14:40:45 -0300

    first-commit

--- src/domains/prontuary/prontuary.controller.ts
+++ src/domains/prontuary/prontuary.controller.ts
@@ -1,32 +1,29 @@
 import { Router, Request, Response } from "express";
-import { create } from "domain";
+import * as path from "path";
+import fs from "fs";
+import Prontuary from "./prontuary.service";
+
+const PATH = path.join(__dirname, "./prontuary_template.json");
 
 const router = Router();
 
-// router.get("/", (req: Request, res: Response) => {
-//   const user_id = Number.isNaN(Number(req.query.user_id)) ? "all" : Number(req.query.user_id);
-//   const title = req.query.title;
-//   const n = Number.isNaN(Number(req.query.n)) ? "all" : Number(req.query.n)
-//   const answer = getUserById(user_id, title, n);
-//   answer.then((rows) => {
-//     res.send(rows);
-//   });
-// });
+router.get("/:id", async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  console.log(id);
+  const prontuary = new Prontuary(id);
+  return res.send(prontuary.content);
+});
 
-// router.post("/create", (req: Request, res: Response) => {
-//   const { user_id, title } = req.body;
-//   const answer = createProfessional(user_id, title);
-//   answer.then((msg) => {
-//     res.send(msg);
-//   });
-// });
-
-// router.post("/delete", (req: Request, res: Response) => {
-//   const { user_id, table_id } = req.body
-//   const answer = deleteProfessional(user_id, table_id)
-//   answer.then((msg) => {
-//     res.send(msg)
-//   })
-// });
+router.post("/:id", async (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+    const id = Number(req.params.id);
+    fs.writeFileSync(id + ".json", JSON.stringify(req.body));
+    return res.status(200).json({ OK: "OK" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("error");
+  }
+});
 
 export default router;
